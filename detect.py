f439477892a2549d244ce16cfa87b22bff9f972a
pengdefeng <2450410477@qq.com>
2025-05-07 11:08:24 +0800

    250epochs参照版本

--- detect.py
+++ detect.py
@@ -7,9 +7,9 @@ import cv2
 if __name__ == '__main__':
 
     # Load a model
-    model = YOLO(model=r'runs\\train\\exp-MobileNetV2\\weights\\best.pt')  
-    model.predict(source=0,
-                  save=False,
-                  show=True,
+    model = YOLO(model=r'runs\\train\\exp2\\weights\\best.pt')  
+    model.predict(source='flower_data\\val\\images\\*.jpg',
+                  save=True,
+                  show=False,
                   )
 
--- train.py
+++ train.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     model.train(data='ultralytics/cfg/datasets/flower.yaml',   # 指定训练数据集的配置文件路径，这个.yaml文件包含了数据集的路径和类别信息
                 cache=False,  # 是否缓存数据集以加快后续训练速度，False表示不缓存
                 imgsz=640,    # 指定训练时使用的图像尺寸，640表示将输入图像调整为640x640像素
-                epochs=200,   # 设置训练的总轮数为200轮
+                epochs=250,   # 设置训练的总轮数为200轮
                 batch=16,      # 设置每个训练批次的大小为16，即每次更新模型时使用16张图片
                 close_mosaic=10,  # 设置在训练结束前多少轮关闭 Mosaic 数据增强，10 表示在训练的最后 10 轮中关闭 Mosaic
                 workers=8,       # 设置用于数据加载的线程数为8，更多线程可以加快数据加载速度
@@ -16,6 +16,6 @@ if __name__ == '__main__':
                 device='0',      # 指定使用的设备，'0'表示使用第一块GPU进行训练
                 optimizer='SGD', #设置优化器为SGD（随机梯度下降），用于模型参数更新
                 project='runs/train',  
-                name='exp-MobileNetV2',
+                name='exp_250',
                 resume=False,
                 )
